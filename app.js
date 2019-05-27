9cefcbeff8fc714787bef6c0a590e4df2f8aca17
Melroy <melsanthmayor87@gmail.com>
2019-05-27 18:38:41 +0530

    Node server app for a list of products, CRUD app

---
+++ app.js
@@ -0,0 +1,36 @@
+// app.js
+
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var product = require('./routes/product'); // Imports routes for the products
+var app = express();
+
+
+// Set up mongoose connection
+var mongoose = require('mongoose');
+//var dev_db_url = 'mongodb+srv://Melroy:mlry&#64mongodbatlas@cluster0-o7kku.mongodb.net/test?retryWrites=true';
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://Melroy:mlry&#64mongodbatlas@cluster0-o7kku.mongodb.net/test?retryWrites=true";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//   const collection = client.db("test").collection("products");
+//   // perform actions on the collection object
+//   client.close();
+// });
+//var mongoDB = process.env.MONGODB_URI || dev_db_url;
+//mongoose.connect(mongoDB);
+mongoose.connect('mongodb://localhost/product', { useNewUrlParser: true });
+mongoose.Promise = global.Promise;
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/products', product);
+
+var port = 1234;
+
+app.listen(port, () => {
+    console.log('Server is up and running on port number ' + port);
+});
